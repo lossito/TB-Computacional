af935f7447ce4594b25c533ac53d2cd2eb505e8a
Piero André Burgos Duncan <u202513368@upc.edu.pe>
2026-09-04 11:40:44 -0500

    Add project files.

---
+++ MyForm1.cpp
@@ -0,0 +1,10 @@
+#include "MyForm1.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+void main() {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew GrafoConexo::MyForm1());
+}
